808181f2892e36885ca04e627f9cddbe5cec842c
VimYoung <bhaumik2386@gmail.com>
2025-03-08 21:50:23 +0530

    minor changes and install script updation

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-]>", "<cmd>tag<CR>")
+vim.keymap.set("n", "[c", function()
+  require("treesitter-context").go_to_context(vim.v.count1)
+end, { silent = true })
--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -7,4 +7,5 @@ return {
   { "folke/which-key.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "ibhagwan/fzf-lua.nvim", enabled = false },
+  { "ray-x/lsp_signature.nvim", enabled = false },
 }
